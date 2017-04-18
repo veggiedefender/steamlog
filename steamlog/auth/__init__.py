278c895d51fd4570b6b12cd9b81379440a355227
Jesse Li <jessejesse123@gmail.com>
2017-04-18 15:45:26 -0400

    micro optimize finding steam id from identity_url

--- steamlog/auth/__init__.py
+++ steamlog/auth/__init__.py
@@ -4,9 +4,6 @@ from steamlog.auth import views
 from steamlog.utils import is_safe_url
 from flask import redirect
 from flask_login import login_user
-import re
-
-_steam_id_re = re.compile("steamcommunity.com/openid/id/(.*?)$")
 
 
 @login_manager.user_loader
@@ -16,8 +13,7 @@ def load_user(user_id):
 
 @oid.after_login
 def create_or_login(resp):
-    match = _steam_id_re.search(resp.identity_url)
-    steam_id = match.group(1)
+    steam_id = resp.identity_url[-17:]
     user = User.get_or_create(steam_id)
     login_user(user, remember=True)
     next_url = oid.get_next_url()

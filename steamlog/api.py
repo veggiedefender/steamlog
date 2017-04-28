94e3af6a38a03fe8b8a3ae5c09f8b657c1bb077a
Jesse Li <jessejesse123@gmail.com>
2017-04-28 11:33:55 -0400

    send dict of game names

--- steamlog/api.py
+++ steamlog/api.py
@@ -7,5 +7,10 @@ from steamlog import app
 @app.route("/api/events/<steam_id>")
 def events(steam_id):
     user = User.query.filter_by(steam_id=steam_id).first_or_404()
-    events = [dict(event) for event in user.game_events]
-    return jsonify(events)
+    game_events = user.game_events
+    events = [dict(event) for event in game_events]
+    names = {event.game.id: event.game.name for event in game_events}
+    return jsonify({
+        "events": events,
+        "names": names
+    })

9d9478a8112f9add3422ea87465914d64ab60bca
Jesse Li <jessejesse123@gmail.com>
2017-04-28 22:03:18 -0400

    add search api endpoint

--- steamlog/api.py
+++ steamlog/api.py
@@ -1,7 +1,8 @@
-from flask import jsonify
+from flask import jsonify, request
 from steamlog.utils import stats
 from steamlog.models import User
-from steamlog import app
+from steamlog import app, db
+from sqlalchemy import or_
 
 
 @app.route("/api/events/<steam_id>")
@@ -14,3 +15,27 @@ def events(steam_id):
         "events": events,
         "names": names
     })
+
+
+@app.route("/api/search", methods=["POST"])
+def search():
+    data = request.get_json()
+    if data is None or data.get("term") is None:
+        return jsonify([])
+    term = data["term"].strip()
+    if len(term) == 0:
+        return jsonify([])
+
+    users = (
+        db.session.query(User)
+        .filter(or_(
+            User.steam_id.contains(term),
+            User.url.contains(term),
+            User.name.contains(term)
+        ))
+        .limit(10)
+        .all()
+    )
+    return jsonify([{
+        "name": user.name
+    } for user in users])

e95be157dfa459e93753f78c29074d53555c8041
Jesse Li <jessejesse123@gmail.com>
2017-05-21 18:53:11 -0400

    Revert "weekday breakdown shows averages"
    This reverts commit 1428c8990153decb08d0a97105f97743e0968923.

--- frontend/src/components/Charts/Weekday.js
+++ frontend/src/components/Charts/Weekday.js
@@ -5,13 +5,9 @@ export default class Weekday extends Component {
   render() {
     let points = this.props.events.reduce((totals, event) => {
       let elapsed = (event.stop_time - event.start_time) / 3600000;
-      totals[event.start_time.getDay()].push(elapsed);
+      totals[event.start_time.getDay()] += elapsed;
       return totals;
-    }, [[],[],[],[],[],[],[]]);
-    points = points.map((hours) => {
-      let total = hours.reduce((total, hour) => total += hour, 0);
-      return total / hours.length;
-    });
+    }, [0,0,0,0,0,0,0]);
 
     const data = {
       labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
